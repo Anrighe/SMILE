dbe0aebf6aa8b5b6bfb604982d6ae799f2d8982d
Anrighe <enrico.marras96@gmail.com>
2024-09-15 17:17:25 +0200

    new updates + insert + delete queries

--- include/DatabaseStatements.hpp
+++ include/DatabaseStatements.hpp
@@ -16,17 +16,18 @@ private:
     std::string getCommandQuery;
 
     /* ---------- INSERT ----------*/
+    std::string insertCommandQuery;
 
+    /* ---------- UPDATE ----------*/
+    std::string updateNewCommandUseQuery;
 
     /* ---------- DELETE ----------*/
+    std::string deleteCommandQuery;
 
     template <typename... Args>
     SQLite::Statement prepareStatement (const std::string& command, Args&&... args) {
-
         SQLite::Statement query(*db, command);
-        std::cout<<"CREATED QUERY " + command + " \n";
         bindArguments(query, 1, std::forward<Args>(args)...);
-        std::cout<<"RETURNING\n";
         return query;
     }
 
@@ -42,18 +43,32 @@ public:
     DatabaseStatements(SQLite::Database& database) { 
         db = &database; 
 
+        /* ---------- CREATE ----------*/
         createHistoryTableQuery = "CREATE TABLE IF NOT EXISTS history (                     \
                                     command VARCHAR(256) PRIMARY KEY,                       \
                                     execution_counter INTEGER DEFAULT 0 NOT NULL,           \
                                     last_execution TEXT DEFAULT 'N/A')";
 
+        /* ---------- SELECT ----------*/
         getAllQuery = "SELECT * FROM history";
 
         getCommandQuery = "SELECT * FROM history WHERE command = '?'";
+
+        /* ---------- INSERT ----------*/
+        insertCommandQuery = "INSERT INTO history VALUES(?, 0, current_timestamp)";
+
+        /* ---------- UPDATE ----------*/
+        updateNewCommandUseQuery = "UPDATE history SET (execution_counter = (SELECT execution_counter FROM history WHERE command = ?), last_execution = current_timestamp) \
+                                    WHERE command = ?";
+
+        /* ---------- DELETE ----------*/
+        deleteCommandQuery = "DELETE FROM history WHERE command = ?";
     }
 
     SQLite::Statement createHistoryTable() { return prepareStatement(createHistoryTableQuery); }
     SQLite::Statement getAll() { return prepareStatement(getAllQuery); }
     SQLite::Statement getCommand() { return prepareStatement(getCommandQuery); }
-
+    SQLite::Statement getInsertCommandQuery() { return prepareStatement(insertCommandQuery); }
+    SQLite::Statement getuUpdateNewCommandUseQuery() { return prepareStatement(updateNewCommandUseQuery); }
+    SQLite::Statement getuDeleteCommandQuery() { return prepareStatement(deleteCommandQuery); }
 };
